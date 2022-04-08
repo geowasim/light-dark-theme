2497ab81693a4f36ae57b91ab48f08645f6483f0
Wasim <waseem_wmgh@yahoo.com>
2022-04-08 18:07:11 +0200

    clean the code

--- script.js
+++ script.js
@@ -8,47 +8,47 @@ const textBox = document.getElementById("text-box");
 
 //Dark or light Images
 
-function changeImageColor(color) {
+function imageMode(color) {
   image1.src = `assets/img/undraw_proud_coder_${color}.svg`;
   image2.src = `assets/img/undraw_feeling_proud_${color}.svg`;
   image3.src = `assets/img/undraw_conceptual_idea_${color}.svg`;
 }
 
-//Dark Mode
-function darkMode() {
-  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  changeImageColor("dark");
-}
-function lightMode() {
-  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  changeImageColor("light");
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? "rgb(0 0 0 / 50%)"
+    : "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgb(255 255 255 / 50%)"
+    : "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  isDark ? imageMode("dark") : imageMode("light");
 }
 
+//Switch Dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-    darkMode();
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
-    lightMode();
+    toggleDarkLightMode(false);
   }
 }
 //Event Listner on theme
 toggleSwitch.addEventListener("change", switchTheme);
 
+//
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme === "dark") {
   document.documentElement.setAttribute("data-theme", "dark");
-  darkMode();
+  toggleDarkLightMode(true);
   if (toggleSwitch.checked === false) {
     toggleSwitch.checked = true;
   }
